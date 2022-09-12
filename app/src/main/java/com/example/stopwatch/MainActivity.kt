8f6fd83d4104f1896f71dd38a3496e005dc14e3a
LeviBClev <levibc06@gmail.com>
2022-09-12 09:02:54 -0700

    added log to lifecycle methods

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -15,10 +15,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onDestroy: ")
     }
 }
