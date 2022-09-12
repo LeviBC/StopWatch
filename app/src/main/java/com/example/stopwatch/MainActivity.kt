2d87a4c004868457c1d004a836ba584ee60c33c2
LeviBClev <levibc06@gmail.com>
2022-09-12 09:53:21 -0700

    added log to lifecycle methods added variables

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.sax.StartElementListener
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     //make a classwide static constant in kotlin
@@ -10,12 +14,18 @@ class MainActivity : AppCompatActivity() {
         //all your static constants go here
         val TAG = "MainActivity"
     }
+    private lateinit var chronometer: Chronometer
+    private lateinit var stop: Button
+    private lateinit var start:Button
+    private lateinit var constraint: ConstraintLayout
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: ")
+        wireWidgets()
     }
 
     override fun onStart() {
@@ -45,5 +55,11 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onDestroy: ")
+    }
+    private fun wireWidgets(){
+        chronometer = findViewById(R.id.chronometer_main_stopwatch)
+        stop = findViewById(R.id.button_main_stop)
+        start = findViewById(R.id.button_main_start)
+        constraint = findViewById(R.id.constraint_main_layout)
     }
 }
